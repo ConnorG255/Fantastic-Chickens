0cb21c3db2669a5450b4487972878cb0863a95e1
conno <25connorgriffin@gmail.com>
2023-12-29 23:23:08 -0800

    UPNP(ipleak :( )

--- scenes/main.gd
+++ scenes/main.gd
@@ -13,7 +13,13 @@ func addPlayer(peer_id):
 	player.name = str(peer_id)
 	get_node("/root/main/Network").add_child(player)
 	
+
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
 	
+
 func _on_join_pressed():
 	main_menu.hide()
 	
@@ -29,9 +35,12 @@ func _on_host_pressed():
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(addPlayer)
-	
+	multiplayer.peer_disconnected.connect(remove_player)
 	addPlayer(multiplayer.get_unique_id())
+	upnp_setup()
 	
-	
-	pass # Replace with function body.
+func upnp_setup():
+	var upnp = UPNP.new()
+	var discoverupnp = upnp.discover()
+	print("Working: " % upnp.query_external_address())
 
